f956d99b62ec1056ad94a21b0afd2d19e3e063d6
0.8lili <130875766+reesenuts@users.noreply.github.com>
2025-03-24 14:02:22 +0800

    landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,23 @@
-<div>
-    <header>
-        <h1>reesenuts.</h1>
+<div class="flex flex-col justify-between h-screen">
+    <header class="mt-10">
+        <p class="font-bold text-2xl" style="color: var(--sinister);">@reesenuts</p>
     </header>
-
-    <main>
-        <h1>Explore knowledge with Reese.ai Chat Bot.</h1>
-        <div>
-            <p>How can I help?</p>
-            <button>insert here</button>
+    
+    <main class="space-y-14 mb-22">
+        <h1 class="text-8xl font-bold text-wrap w-2/2" style="color: var(--sinister);">Explore knowledge with Reese.ai Chat Bot.</h1>
+        <div class="w-1/2 p-2 rounded-full flex justify-between items-center" style="background-color: var(--silk);">
+            <p class="text-sm pl-6" style="color: var(--bedrock);">How can I help?</p>
+            <!-- svelte-ignore a11y_consider_explicit_label -->
+            <a href="/chatbot" class="p-4 rounded-full cursor-pointer" style="background-color: var(--placebo);">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#2383E2" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-right">
+                    <path d="M5 12h14"/>
+                    <path d="m12 5 7 7-7 7"/>
+                </svg>
+            </a>
         </div>
     </main>
 
-    <footer>
-        <p>Powered by <span>Svelte</span></p>
+    <footer class="mb-10">
+        <p class="text-xs" style="color: var(--bedrock);">Powered by <span class="font-semibold">Svelte</span></p>
     </footer>
 </div>
---
+++ src/routes/chatbot/+page.svelte
@@ -0,0 +1,1 @@
+<p>chatbot page.</p>
