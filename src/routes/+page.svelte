c9390afa73a32e03688aaffa8b37d297853c2675
0.8lili <130875766+reesenuts@users.noreply.github.com>
2025-03-24 13:17:15 +0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<div>
+    <header>
+        <h1>reesenuts.</h1>
+    </header>
+
+    <main>
+        <h1>Explore knowledge with Reese.ai Chat Bot.</h1>
+        <div>
+            <p>How can I help?</p>
+            <button>insert here</button>
+        </div>
+    </main>
+
+    <footer>
+        <p>Powered by <span>Svelte</span></p>
+    </footer>
+</div>
